24509bd2a7a4ab1664e3f87c014a34a6989e0f0e
Khai Truong <kd.kidr@gmail.com>
2021-03-23 14:29:17 +0700

    Open file

--- assignment1_group26.cpp
+++ assignment1_group26.cpp
@@ -9,8 +9,14 @@ int main(int argc, char* argv[])
 {
 	if (argc != 2) {
 		cout << "Please provide exactly 1 file name for this program";
-		return -1;
+		return 0;
 	}
 	char* file_name = argv[1];
 	std::cout << file_name << endl;
+	ifstream infile(file_name);
+	if (!infile)
+	{
+		cout << "Cannot open file " << file_name << endl;
+		return 0;
+	}
 }
